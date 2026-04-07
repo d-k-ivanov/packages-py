ba0c31daa08c550263ad508ddb74811cbc8d7e2d
Dmitry Ivanov <d.k.ivanov@live.com>
2026-04-07 02:26:22 +0200

    sync repo with MiroPsota/torch_packages_builder

--- .github/workflows/cuda/Windows_install.sh
+++ .github/workflows/cuda/Windows_install.sh
@@ -12,7 +12,7 @@ declare -A CUDA_LINKS=(
   ["cu130"]="https://developer.download.nvidia.com/compute/cuda/13.0.2/local_installers/cuda_13.0.2_windows.exe"
 )
 CUDA_LINK=${CUDA_LINKS[$CUDA_VERSION]}
-s
+
 CUDA_SHORT=${CUDA_VERSION:2:2}.${CUDA_VERSION:4:1}
 curl -k -L $CUDA_LINK --output _cuda_installer.exe
 
